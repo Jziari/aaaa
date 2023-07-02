16d452459ef959eea113aa30a2a6012f07c9e363
Jziari <jahonziari@gmail.com>
2023-07-01 23:42:40 -0700

    aaa

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Jahon Ziari
+# GitHub username: Jziari
+# Date: 07/01/2023
+# Description: Asks the user for their favorite animal and then prints out that it is their favorite animal
+
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
