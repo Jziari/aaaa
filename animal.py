2a19e3dba1c82bf25e6e333dfdb15b9ec4d0ec85
Jziari <jahonziari@gmail.com>
2023-07-01 23:49:43 -0700

    aaa

--- animal.py
+++ animal.py
@@ -4,5 +4,6 @@
 # Description: Asks the user for their favorite animal and then prints out that it is their favorite animal
 
 
+
 fave_animal = input("Please enter your favorite animal: ")
 print("Your favorite animal is the " + fave_animal + ".")
